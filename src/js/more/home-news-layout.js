8edd86a6c28ebb5bc9335ec5f7d625391a226483
Denisse Daugaard <you@example.com>
2025-04-29 08:48:57 +0200

    swipe funtion_1

--- src/js/more/home-news-layout.js
+++ src/js/more/home-news-layout.js
@@ -1,18 +1,23 @@
 
 
 export default function newsBody(dataOrigin) {
+
+    // dataOrigin.forEach( category=> {
+    //     console.log(category.disabled);
+    // })
    
     let articleElm = document.createElement("article")
     articleElm.classList.add("news")
     articleElm.innerHTML= `
       ${dataOrigin.map( category => `
-       <section class="news__articles">
+       <section class="news__articles" ${category.disabled ? 'style="display: none;"' : ''}>
               <details name="categories">
                   <summary class="news__header">
                   <img class="logo" src="/img/newsify_logo.svg" alt="LOGO">
                   <h3 class="news__category">${category.section}</h3></summary>
                   ${category.articles.map(news => `
                       <div class="news__content">
+                        <div class="swipe__content">
                           <figure class="news__img__container">
                               <img class="news__img" src="${news.multimedia?.[1]?.url || '/public/img/fallback.jpg'}" alt="${news.title}">
                           </figure>
@@ -21,21 +26,13 @@ export default function newsBody(dataOrigin) {
                               <h5>${news.title}</h5>
                               <p>${news.abstract}</p>
                           </section>
+                        </div>  
                       </div>    
                   `).join("")}
               </details>
           </section>
       `).join("")}
     `
-    dataOrigin.forEach( category => {
-        console.log(category.disabled);
-        if(category.disabled){
-            console.log("this is disabled")
-        }else{
-            console.log("this is NOT disabled")
-        }
-    })
-    
     document.querySelector("main").append(articleElm)
     
     
